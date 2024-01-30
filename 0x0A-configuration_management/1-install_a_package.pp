74732772e00c98cdf967ddd0dc4e5421af1e7c1f
leone-nyaga <leonenyaga@gmail.com>
2024-01-30 05:45:55 -0800

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 
-package { 'python3-pip':
-  ensure => present,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  creates     => '/usr/local/bin/flask',
+package { 'flask':
+	ensure => '2.1.0',
+	provider => 'pip'
 }
