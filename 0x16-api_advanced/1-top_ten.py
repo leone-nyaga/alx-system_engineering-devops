51dbbbd61fd6a7d388ec2338939b06a2320b2251
leone-nyaga <leonenyaga@gmail.com>
2024-05-09 06:02:08 -0700

    reddit api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,21 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import json
 import requests
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        print(None)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-
-    data = response.json()
-    if 'error' in data or 'data' not in data:
-        print(None)
-        return
-
-    posts = data['data']['children']
-    for post in posts:
-        print(post['data']['title'])
-
-# Example usage
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
-
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
